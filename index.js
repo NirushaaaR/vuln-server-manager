a0301ee34b1a4998ff9ec5e78dc209a0558e9e83
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2021-02-13 11:39:37 +0000

    move docker path

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const CORS = require('cors');
-const os = require('os');
-const { execFile } = require("child_process");
 
 app.use(express.json());
 
 // app.use(CORS());
 const allowedList = ["http://localhost:8000", "http://127.0.0.1:8000"];
-app.use(CORS({
+const corsOption = {
     origin: function (origin, cb) {
         if (allowedList.indexOf(origin) !== -1) {
             cb(null, true);
@@ -16,135 +14,9 @@ app.use(CORS({
             cb(new Error('Error'));
         }
     }
-}));
-
-const { Docker } = require('./Model');
-
-app.post("/docker", async (req, res) => {
-    const { port, docker } = req.body;
-
-    if (port !== undefined && docker !== undefined) {
-        // beware command injection !!!!
-        const command = `${__dirname}/manage-docker.sh`;
-        const args = ['deploy', docker, port];
-
-        let instance = await Docker.findOne({ where: { name: docker } });
-
-        if (instance !== null) {
-            // check port
-            if (instance.port !== null) {
-                return res.status(400).json({status: `Deploy '${docker}' ใน port ${instance.port} ไปแล้ว กรุณาลบแล้วทำใหม่`});
-            }
-
-            instance.isOperating = true;
-            await instance.save();
-
-        } else {
-            instance = await Docker.create({
-                name: docker,
-                status: "deploying"
-            });
-        }
-
-        execFile(command, args, async (err, stdout, stderr) => {
-
-            if (err) {
-                console.log("err", err);
-                instance.status = `Error: ${err}`;
-            } else if (stderr) {
-                if (stderr.indexOf("repository does not exist") >= 0) {
-                    instance.status = "ไม่พบ docker repo นี้";
-                } else if (stderr.indexOf("port is already allocated") >= 0) {
-                    instance.status = "deploy ด้วย port นี้ไปแล้ว";
-                } else if (stderr.indexOf("Downloaded newer image for") >= 0) {
-                    instance.port = port;
-                    instance.status = "deployed";
-                } else {
-                    instance.status = stderr;
-                }
-            }
-            else if (stdout) {
-                instance.port = port;
-                instance.status = "deployed";
-            }
-            
-            instance.isOperating = false;
-            await instance.save();
-        });
-
-        return res.json({ "status": "deploying docker" });
-    }
-
-    return res.status(400).json({ "status": "input ผิด ({docker, port})" });
-});
-
-
-app.get("/docker/:docker", async (req, res) => {
-    const docker = req.params.docker;
-    const instance = await Docker.findOne({ where: {name: docker} });
-    if (instance !== null) {
-        const eth0 = os.networkInterfaces()['eth0']
-        return res.json({ "data": instance, "url": `http://${eth0 ? eth0[0].address : "workspace"}:${instance.port}` });
-    } else {
-        return res.status(404).json({ "status": "ไม่มีใน Server" });
-    }
-});
-
-app.delete("/docker/:docker", async (req, res) => {
-    const docker = req.params.docker;
-
-    const instance = await Docker.findOne({ where: {name: docker} });
-
-    if (instance !== null) {
-        const command = `${__dirname}/manage-docker.sh`;
-        const args = ['delete', docker, "bruh"];
-
-        instance.isOperating = true;
-        instance.status = "กำลัง Delete";
-        await instance.save();
-
-        execFile(command, args, async (err, stdout, stderr) => {
-
-            try {
-
-                if (err) {
-                    console.log("err", err);
-                    instance.status = "fail to delete";
-                } else if (stderr) {
-                    if (stderr.indexOf("Remove one or more containers") >= 0) {
-                        instance.port = null;
-                        instance.status = "remove from server";
-                    } else {
-                        instance.status = stderr;
-                    }
-                } else if (stdout) {
-                    instance.port = null;
-                    instance.status = "remove from server"
-                }
-
-            } catch (error) {
-                instance.status = error.message;
-            }
-
-            console.log("stdout", stdout);
-            console.log("stderr", stderr);
-
-            instance.isOperating = false;
-            await instance.save();
-        });
-
-        return res.json({ "status": "deleting the container" });
-
-    } else {
-        return res.status(404).json({ "err": "ไม่มีอยู่ใน Server" });
-    }
-
-});
-
-// handle error
-app.use("*", (req, res) => {
-    res.end();
-})
+}
+// dont forget to add cors option
+app.use("/manage", CORS(corsOption), require('./docker'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("server start"));
